2ba55d07260af5bda5332a294a655fef8f2ab1c2
fl77ex <flexdisc.03@gmail.com>
2025-02-09 17:24:36 +0100

    Create machine_id_changer.sh

---
+++ machine_id_changer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Обновление системы
+echo "Обновление системы..."
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
+
+# Замена machine-id
+echo "Изменение machine-id..."
+
+# Удаление текущего machine-id
+sudo rm /etc/machine-id
+
+# Генерация нового machine-id
+sudo systemd-machine-id-setup
+
+# Перезагрузка системы
+echo "Перезагрузка системы..."
+sudo reboot
